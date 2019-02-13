0ea202997449ed515f19e13f6bb50ce9a5866506
Geoff Hankerson <ghankerson@mor.org>
2019-02-13 11:28:22 -0600

    fix fixture

--- src/lib/components/Body/test/MultipleMarks.test.js
+++ src/lib/components/Body/test/MultipleMarks.test.js
@@ -12,14 +12,7 @@ test("It renders a marks correctly", () => {
   ReactDOM.render(<Body nodeData={description} />, container);
   expect(container.firstChild.tagName).toEqual("P");
   expect(container.firstChild.innerHTML).toEqual(
-    singleLineString(
-      `This week: a live broadcast from the Fitzgerald Theater in Saint Paul, Minnesota,
-    with our host <a href=\"https://www.facebook.com/ChrisThile\"><strong>Chris Thile</strong></a>,
-    and special guests <a href=\"http://www.nathanielrateliff.com/\"><strong>Nathaniel Rateliff &amp; The Night Sweats</strong></a>,
-    <a href=\"http://www.anaismitchell.com/\"><strong>Anais Mitchell</strong></a>,
-    and <a href=\"http://www.johnhodgman.com/\"><strong>John Hodgman</strong></a>.
-    <a href=\"https://www.prairiehome.org/shows/52951\">More from this episode</a>`
-    )
+    `This week: a live broadcast from the Fitzgerald Theater in Saint Paul, Minnesota, with our host <a href=\"https://www.facebook.com/ChrisThile\"><strong>Chris Thile</strong></a>, and special guests <a href=\"http://www.nathanielrateliff.com/\"><strong>Nathaniel Rateliff &amp; The Night Sweats</strong></a>, <a href=\"http://www.anaismitchell.com/\"><strong>Anais Mitchell</strong></a>, and <a href=\"http://www.johnhodgman.com/\"><strong>John Hodgman</strong></a>. <a href=\"https://www.prairiehome.org/shows/52951\">More from this episode</a>`
   );
 });
 
