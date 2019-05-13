433afab06d620d5925ea25a4d4b177068c55f8c7
Kim Thompson <kthompson@mpr.org>
2019-05-13 13:00:11 -0500

    eslint failed in a lot of places, which I may fix later

--- src/lib/atoms/ApmGallery/ApmGallery.js
+++ src/lib/atoms/ApmGallery/ApmGallery.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image } from "apm-mimas";
+import { Slideshow } from "apm-enceladus";
 import ApmImage from '../ApmImage/ApmImage'
 import uuid from "uuid";
 
@@ -10,20 +11,24 @@ class ApmGallery extends React.Component {
 
   render() {
     const slides = this.props.nodeData.content;
+    // console.log(slides);
     return (
       <div className="apm-gallery">
-        <div className="apm-gallery-title">
+        <div className="apm-gallery_title">
           {this.props.nodeData.attrs.title}
         </div>
-        <ul className="apm-gallery-slides">
+        {/* <ul className="apm-gallery-slides">
           {slides.map(slide => {
             return (
               <li className="apm-slide" key={uuid()}>
-                <ApmImage image={slide.attrs} embedded={this.props.embedded} />
+                <ApmImage image={slide.attrs} embedded={this.props.embedded} s/>
               </li>
             );
           })}
-        </ul>
+        </ul> */}
+        <div className="apm-gallery_slides">
+          <Slideshow images={slides} animation="slide" />
+        </div>
       </div>
     );
   }
