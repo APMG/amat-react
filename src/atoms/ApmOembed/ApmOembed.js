01acc6e6097bb9e3fa1b80adc34ac8072ec7c820
Geoff Hankerson <ghankerson@mor.org>
2019-07-12 11:55:47 -0500

    update version  and for react oembed container

--- src/atoms/ApmOembed/ApmOembed.js
+++ src/atoms/ApmOembed/ApmOembed.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import EmbedContainer from 'react-oembed-container';
+import EmbedContainer from '@apmg/react-oembed-container';
 
 const ApmOembed = (props) => {
   if (props.minimal) {
