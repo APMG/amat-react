a017a9ed798b8df483d6b4ddae50c9583ab0de9c
Kim Thompson <kthompson@mpr.org>
2019-07-22 17:13:55 -0500

    finally getting somewhere

--- src/atoms/CustomHtml/CustomHtml.js
+++ src/atoms/CustomHtml/CustomHtml.js
@@ -2,20 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import Sidechain from '@nprapps/sidechain';
 
-// This component should strip out scripts and place them in a responsive iFrame (i.e. "fauxembedding"). It should just display manually-created iframes wholesale.
+// This component should strip out scripts but allow iFrames
 
 // In the future we may want to just add "responsive iFrame" as an Amat object that reporters can just use, separating it from the CustomHtml stuff.
 
 class CustomHtml extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      html: ''
+    };
+  }
+
+  componentDidMount() {
+    // parse the string
+    let doc = new DOMParser().parseFromString(
+      this.props.nodeData.attrs.html,
+      'text/html'
+    );
+
+    console.log(this.props.nodeData);
+
+    // debugger;
+
+    // store scripts in script tags to run later
+    let scripts = doc.querySelectorAll('script');
+    let iframes = doc.querySelectorAll('iframe');
+
+    // if there are any script tags (i.e. not in iframes), use fallback
+
+    // remove scripts from markup itself
+    // [].forEach.call(scripts, function(script) {
+    //   script.parentNode.removeChild(script);
+    // });
+
+    // let string = new XMLSerializer().serializeToString(doc);
+
+    // debugger;
+    let html =
+      scripts.length > 0 && iframes.length === 0
+        ? `<iframe class="features-marketplace document" width="100%" height="650px" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="${this.props.nodeData.attrs.fallback_url}"></iframe>`
+        : this.props.nodeData.attrs.html;
+
+    this.setState({
+      html: html
+    });
   }
 
   render() {
-    const markup = { __html: this.props.nodeData.attrs.html };
-    console.log(this.props.nodeData);
+    if (this.props.minimal) {
+      return null;
+    }
 
-    return <div dangerouslySetInnerHTML={markup} />;
+    const markup = { __html: this.state.html };
+
+    return <div className="customHtml" dangerouslySetInnerHTML={markup} />;
   }
 }
 
