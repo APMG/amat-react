3adf048c63090877c3c73f86b0535f93bbe0bf2d
astevenson <astevenson@mpr.org>
2019-07-31 13:51:44 -0500

    add a test around titles in links

--- src/atoms/Text/Link.test.js
+++ src/atoms/Text/Link.test.js
@@ -17,6 +17,7 @@ const doc = {
             {
               type: 'link',
               attrs: {
+                className: 'default',
                 href:
                   'https://www.americanpublicmedia.org/apm-research-lab-finds-37-percent-of-americans-more-likely-to-view-minneapolis-as-a-desirable-destination-following-super-bowl-lii-coverage/',
                 title: ''
@@ -30,11 +31,48 @@ const doc = {
   ]
 };
 
+const docWithTitle = {
+  type: 'doc',
+  version: 1,
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          marks: [
+            {
+              type: 'link',
+              attrs: {
+                className: 'default',
+                href:
+                  'https://www.americanpublicmedia.org/apm-research-lab-finds-37-percent-of-americans-more-likely-to-view-minneapolis-as-a-desirable-destination-following-super-bowl-lii-coverage/',
+                title: 'title'
+              }
+            }
+          ],
+          text: 'READ THE PRESS RELEASE'
+        }
+      ]
+    }
+  ]
+};
+
+
 test('It renders a link in a paragraph', () => {
   const { container } = render(<Body nodeData={doc} />);
 
   expect(container.firstChild.tagName).toEqual('P');
   expect(container.firstChild.innerHTML).toEqual(
-    `<a href="https://www.americanpublicmedia.org/apm-research-lab-finds-37-percent-of-americans-more-likely-to-view-minneapolis-as-a-desirable-destination-following-super-bowl-lii-coverage/">READ THE PRESS RELEASE</a>`
+    `<a href="https://www.americanpublicmedia.org/apm-research-lab-finds-37-percent-of-americans-more-likely-to-view-minneapolis-as-a-desirable-destination-following-super-bowl-lii-coverage/" class="default">READ THE PRESS RELEASE</a>`
+  );
+});
+
+test('It renders a link in a paragraph with a title', () => {
+  const { container } = render(<Body nodeData={docWithTitle} />);
+
+  expect(container.firstChild.tagName).toEqual('P');
+  expect(container.firstChild.innerHTML).toEqual(
+    `<a href="https://www.americanpublicmedia.org/apm-research-lab-finds-37-percent-of-americans-more-likely-to-view-minneapolis-as-a-desirable-destination-following-super-bowl-lii-coverage/" title="title" class="default">READ THE PRESS RELEASE</a>`
   );
 });
