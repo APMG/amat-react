351c8838e0a62842bccfed91cf1d141dce38a509
Philip Kooistra <pkooistra@mpr.org>
2020-01-06 12:02:57 -0600

    Removing non-breaking space from Text component

--- src/atoms/Text/Text.js
+++ src/atoms/Text/Text.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Text = (props) => {
-  const txt = props.nodeData['text'].replace(/\B /g, '\u00a0');
+  const txt = props.nodeData['text'];
   return <>{txt}</>;
 };
 
