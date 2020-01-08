cc3b570e5ceaaf7d6e0f7c0faafd49c18356c164
astevenson <astevenson@mpr.org>
2020-01-08 11:42:12 -0600

    Aside handler #40995

---
+++ src/atoms/ApmAside/ApmAside.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Body from '../../components/Body/Body';
+
+afterEach(cleanup);
+
+const doc = {
+  type: 'doc',
+  version: 1,
+  content: [
+    {
+      type: 'apm_aside',
+      content: [
+        {
+          content: [
+            {
+              text: 'An aside.',
+              type: 'text'
+            }
+          ],
+          type: 'paragraph'
+        }
+      ]
+    }
+  ]
+};
+
+test('It renders a body from a Prosemirror doc', () => {
+  const { container } = render(<Body nodeData={doc} />);
+
+  expect(container.innerHTML).toEqual('<aside><p>An aside.</p></aside>');
+});
