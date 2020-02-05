ba6cabd5f01fb4712fa4130b91610e3608201dcf
Kim Thompson <kthompson@mpr.org>
2020-02-05 11:46:01 -0600

    reworked the logic of the ApmOembed component and made the failure node more informative

--- src/atoms/ApmOembed/ApmOembed.js
+++ src/atoms/ApmOembed/ApmOembed.js
@@ -1,50 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import EmbedContainer from '@apmg/react-oembed-container';
 
 const ApmOembed = (props) => {
-  useEffect(() => {
-    embed = findEmbedded();
-    if (embed.provider_name === 'NPR') {
-      import('@nprapps/sidechain');
-    }
-  });
-
   if (props.minimal) {
     return null;
   }
+
   const findEmbedded = () => {
-    const ret = props.embedded.oembeds.find((embed) => {
-      return embed.url === props.nodeData.attrs.src;
-    });
-    return ret;
+    return props.embedded.oembeds.find(
+      (embed) => embed.url === props.nodeData.attrs.src
+    );
   };
 
   const markup = (rawMarkup) => {
     return { __html: rawMarkup.replace(/\n/g, '') };
   };
 
-  let embed;
-  try {
-    embed = findEmbedded();
-    if (!embed.html) return props.fallback_text;
-  } catch (err) {
-    return <></>;
+  const [embed, setEmbed] = useState(null);
+
+  useEffect(() => {
+    setEmbed(findEmbedded());
+    if (embed != null && embed.provider_name === 'NPR') {
+      import('@nprapps/sidechain');
+    }
+  });
+
+  if (embed == null || embed == undefined) {
+    return (
+      <div className="amat-oembed missing" data-url={props.nodeData.attrs.src}>
+        <a href={props.nodeData.attrs.src}>#{props.fallback_text}</a>
+      </div>
+    );
+  } else {
+    const cname =
+      embed && embed.provider_name
+        ? embed.provider_name.toLowerCase().replace(/\s/g, '')
+        : '';
+    const html = markup(embed.html);
+    return (
+      <EmbedContainer markup={embed.html}>
+        <div
+          className={`amat-oembed ${cname}`}
+          data-url={embed.url}
+          dangerouslySetInnerHTML={html}
+        />
+      </EmbedContainer>
+    );
   }
-  const cname =
-    embed && embed.provider_name
-      ? embed.provider_name.toLowerCase().replace(/\s/g, '')
-      : '';
-  const html = markup(embed.html);
-  return (
-    <EmbedContainer markup={embed.html}>
-      <div
-        className={`amat-oembed ${cname}`}
-        data-url={embed.url}
-        dangerouslySetInnerHTML={html}
-      />
-    </EmbedContainer>
-  );
 };
 
 ApmOembed.propTypes = {
