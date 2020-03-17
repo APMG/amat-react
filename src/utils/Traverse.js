432ed73dd1cd071caf3ec67f1e4620ad7bd00ebe
Philip Kooistra <pkooistra@mpr.org>
2020-03-17 14:25:26 -0500

    Refactor of traverse

--- src/utils/Traverse.js
+++ src/utils/Traverse.js
@@ -3,27 +3,20 @@ import WrapInMarks from './WrapInMarks';
 import { v4 as uuid } from 'uuid';
 import ApmTableOfContents from '../atoms/ApmTableOfContents/ApmTableOfContents';
 
-// Not really happy with the structure of this but spent some time trying to refactor with no luck.
-// Will settle for working code for now. Happy to hear your suggestions - GH
 const Traverse = (props) => {
-  let arr = [];
-  if (props?.nodeData?.content) {
-    for (let i = 0; i < props.nodeData.content.length; ++i) {
-      if (props.nodeData.content[i].type === 'apm_table_of_contents') {
-        arr.push(
-          <ApmTableOfContents
-            nodeData={props.nodeData.content}
-            key={uuid()}
-            components={props.components}
-          />
-        );
-        break;
-      }
-      arr.push(WrapInMarks(props.nodeData.content[i], props));
-    }
+  if (props.nodeData?.content[0]?.type === 'apm_table_of_contents') {
+    return (
+      <ApmTableOfContents
+        nodeData={props.nodeData.content}
+        key={uuid()}
+        components={props.components}
+      />
+    );
   }
 
-  return arr;
+  return props.nodeData?.content?.map((item) => {
+    return WrapInMarks(item, props);
+  });
 };
 
 export default Traverse;
