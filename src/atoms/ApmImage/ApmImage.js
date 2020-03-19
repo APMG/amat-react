69ac40fb41571c41722f6eca6199516523b074ff
Kim Thompson <kthompson@mpr.org>
2020-03-19 14:42:06 -0500

    prettier

--- src/atoms/ApmImage/ApmImage.js
+++ src/atoms/ApmImage/ApmImage.js
@@ -51,19 +51,12 @@ const ApmImage = (props) => {
       } else {
         if (props?.image?.credit && props.image.credit_url) {
           return (
-            <a
-              href={props.image.credit_url}
-              className="figure_credit"
-            >
+            <a href={props.image.credit_url} className="figure_credit">
               {props.image.credit}
             </a>
           );
         } else if (props?.image?.credit) {
-          return (
-            <div className="figure_credit">
-              {props.image.credit}
-            </div>
-          );
+          return <div className="figure_credit">{props.image.credit}</div>;
         }
       }
     }
@@ -118,9 +111,7 @@ const ApmImage = (props) => {
         {props?.image?.long_caption || props?.image?.credit ? (
           <figcaption className="figure_caption">
             {props.image.long_caption && (
-              <div className="figure_text">
-                {props.image.long_caption}
-              </div>
+              <div className="figure_text">{props.image.long_caption}</div>
             )}
             {captionCredit()}
           </figcaption>
