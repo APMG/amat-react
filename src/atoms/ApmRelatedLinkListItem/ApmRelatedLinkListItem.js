91de7e64e72e380d35327ccc4ad6b0be1b01d9c7
Lilith Bentley <lbentley@mpr.org>
2020-03-20 15:02:59 -0500

    Add AMP styles to prefix+link combinations

--- src/atoms/ApmRelatedLinkListItem/ApmRelatedLinkListItem.js
+++ src/atoms/ApmRelatedLinkListItem/ApmRelatedLinkListItem.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const ampStyles = {
+  prefix: {
+    display: 'inline-block',
+    marginRight: '.5em',
+    fontWeight: '700'
+  },
+  link: {
+    color: '#00334e',
+    fontWeight: '700',
+    margin: '0 0 0.5em'
+  }
+};
+
 const ApmRelatedLinkListItem = (props) => {
   const { prefix, title, url } = props.nodeData.attrs;
-  return (
-    <li className="apm-related-link">
-      <span className="apm-related-link-prefix">{prefix}</span>
-      <a href={url}>{title}</a>
-    </li>
-  );
+  if (props.isAmp) {
+    return (
+      <li className="apm-related-link">
+        <span style={ampStyles.prefix} className="apm-related-link-prefix">
+          {prefix}
+        </span>
+        <a style={ampStyles.link} href={url}>
+          {title}
+        </a>
+      </li>
+    );
+  } else {
+    return (
+      <li className="apm-related-link">
+        <span className="apm-related-link-prefix">{prefix}</span>
+        <a href={url}>{title}</a>
+      </li>
+    );
+  }
 };
 
 ApmRelatedLinkListItem.propTypes = {
+  isAmp: PropTypes.bool,
   nodeData: PropTypes.object
 };
 
