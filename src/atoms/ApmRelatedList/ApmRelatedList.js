163c4f19fba5e206e64f0b22c8cda4dd44c5b91f
Lilith Bentley <lbentley@mpr.org>
2020-03-20 14:54:08 -0500

    Add inline styles to AMP related lists

--- src/atoms/ApmRelatedList/ApmRelatedList.js
+++ src/atoms/ApmRelatedList/ApmRelatedList.js
@@ -2,6 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Traverse from '../../utils/Traverse';
 
+const ampStyles = {
+  relatedList: {
+    margin: '2em 0',
+    fontFamily: 'Roboto, system-ui, -apple-system, sans-serif',
+    clear: 'both'
+  },
+  relatedListTitle: {
+    fontSize: '1.5em',
+    fontWeight: '700',
+    margin: '0 0 0.5em'
+  }
+};
+
 const enlistify = (props) => {
   if (props?.nodeData?.content) {
     for (let i = 0; i < props.nodeData.content.length; ++i) {
@@ -16,15 +29,25 @@ const enlistify = (props) => {
 const ApmRelatedList = (props) => {
   if (props.minimal) return null;
   const { title } = props.nodeData.attrs;
-  return (
-    <div className="apm-related-list">
-      <div className="apm-related-list-title">{title}</div>
-      <ul className="apm-related-list-body">{Traverse(enlistify(props))}</ul>
-    </div>
-  );
+  if (props.isAmp) {
+    return (
+      <div style={ampStyles.relatedList} className="apm-related-list">
+        <div style={ampStyles.relatedListTitle} className="apm-related-list-title">{title}</div>
+        <ul className="apm-related-list-body">{Traverse(enlistify(props))}</ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="apm-related-list">
+        <div className="apm-related-list-title">{title}</div>
+        <ul className="apm-related-list-body">{Traverse(enlistify(props))}</ul>
+      </div>
+    );
+  }
 };
 
 ApmRelatedList.propTypes = {
+  isAmp: PropTypes.bool,
   minimal: PropTypes.bool,
   nodeData: PropTypes.object
 };
