8f33353183be044ed6059ef2b99e0877955d7745
Philip Kooistra <pkooistra@mpr.org>
2020-08-20 16:38:54 -0500

    Adding export of ApmOembed to fix bug in kids-podcasts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Traverse from './utils/Traverse';
 import Body from './components/Body/Body';
+import ApmOembed from './atoms/ApmOembed/ApmOembed';
 import EscapeSpecialCharaters from './utils/EscapeSpecialCharaters';
 
-export { Traverse, Body, EscapeSpecialCharaters };
+export { Traverse, Body, ApmOembed, EscapeSpecialCharaters };
