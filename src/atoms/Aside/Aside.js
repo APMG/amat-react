949c1dfdedbc8d27e54db08bbe7043a9afa74cfa
Phanx091 <jason.jayphan@gmail.com>
2025-09-11 16:44:53 -0500

    inject styles to head

--- src/atoms/Aside/Aside.js
+++ src/atoms/Aside/Aside.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Traverse from '../../utils/Traverse';
 
 const Aside = (props) => {
-  return (
-    <>
-      <style>
-        {`
-          .apm-aside-custom {
-            float: right;
-            width: 300px;
-            margin: 0 0 20px 20px;
-            padding: 0px 15px;
-            background-color: #f8f9fa;
-            color: #000;
-            border: 1px solid #dee2e6;
-            border-radius: 8px;
-            line-height: 1.4;
-          }
+  useEffect(() => {
+    // Inject styles into document head when component mounts
+    // This ensures styles work when used as an npm package
+    const styleId = 'apm-aside-styles';
 
-          .apm-aside-custom p {
-            font-size: 13px;
+    if (!document.getElementById(styleId)) {
+      const style = document.createElement('style');
+      style.id = styleId;
+      style.textContent = `
+        .apm-aside-custom {
+          float: right;
+          width: 300px;
+          margin: 0 0 20px 20px;
+          padding: 0px 15px;
+          background-color: #f8f9fa;
+          color: #000;
+          border: 1px solid #dee2e6;
+          border-radius: 8px;
+          line-height: 1.4;
+        }
+
+        .apm-aside-custom p {
+          font-size: 13px;
+        }
+        
+        @media (max-width: 957px) {
+          .apm-aside-custom {
+            float: none;
+            width: 100%;
+            margin: 20px 0;
           }
-          
-          @media (max-width: 957px) {
-            .apm-aside-custom {
-              float: none;
-              width: 100%;
-              margin: 20px 0;
-            }
-          }
-        `}
-      </style>
-      <aside className="apm-aside-custom">{Traverse(props)}</aside>
-    </>
-  );
+        }
+      `;
+      document.head.appendChild(style);
+    }
+  }, []);
+
+  return <aside className="apm-aside-custom">{Traverse(props)}</aside>;
 };
 
 export default Aside;
